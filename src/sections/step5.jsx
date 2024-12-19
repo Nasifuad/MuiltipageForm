61eb821e8759b93358b8dc55ac22bbb0ca4908fc
Nasif <nasifuad007@gmail.com>
2024-12-19 08:06:42 +0600

    All done

--- src/sections/step5.jsx
+++ src/sections/step5.jsx
@@ -1,19 +1,15 @@
 import ok from "../assets/images/icon-thank-you.svg";
-export default function Step4() {
+export default function Step5() {
   return (
     <>
-      <div className=" flex flex-col p-10 gap-4   sm:translate-y-[-50px] bg-white ">
-        <div className="flex flex-col justify-center items-center gap-4">
-          <img src={ok} alt="" />
-          <h1 className="text-4xl text-Marine-blue  font-bold font-ubuntu">
-            Thank You!
-          </h1>
-          <p className="text-center text-Cool-gray w-1/2">
-            Thanks for confirming your subscription! We hope you have fun using
-            our platform. If you ever need support, please feel free to email us
-            at support@loremgaming.com
-          </p>
-        </div>
+      <div className=" flex flex-col p-10 gap-4   sm:translate-y-[-50px] justify-center items-center bg-white ">
+        <img src={ok} alt="OK" />
+        <h1 className="text-3xl font-bold font-ubuntu">Thank You !</h1>
+        <p className=" text-Cool-gray w-[300px]">
+          Thanks for confirming your subscription! We hope you have fun using
+          our platform. If you ever need support, please feel free to email us
+          at 7ZV4y@example.com
+        </p>
       </div>
     </>
   );

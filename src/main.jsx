6301fa0f19a551c1478a0eb26a3269157c9a02a6
Nasif <nasifuad007@gmail.com>
2024-12-28 19:15:25 +0600

    s

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
-
+// sdl;fjlksdjhfksdhfjkdsh
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
